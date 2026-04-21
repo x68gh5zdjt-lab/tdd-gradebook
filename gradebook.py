ce9d1d5b585e5c0a39067c0706e886e21e810845
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-04-21 08:31:00 -0400

    Fully implimented letter grade

--- gradebook.py
+++ gradebook.py
@@ -1,5 +1,14 @@
 def letter_grade(score):
-    return "A"
+    if score >= 90: 
+        return "A"
+    elif score >= 80: 
+        return "B"
+    elif score >= 70: 
+        return "C"
+    elif score >= 60: 
+        return "D"
+    else:
+         return "F"
 
 def is_passing(score):
     pass
