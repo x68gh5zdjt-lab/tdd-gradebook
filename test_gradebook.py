4b8bafe37cd0eca473c5c8ef7c3b34dde8d1dd28
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-04-21 08:25:37 -0400

    Grade never before seen at wecib

--- test_gradebook.py
+++ test_gradebook.py
@@ -1,5 +1,8 @@
 from gradebook import letter_grade
+from gradebook import is_passing
 
 def test_letter_grade_A():
     assert letter_grade(95) == "A"
-    
+
+def test_letter_grade_F():
+    assert letter_grade(42) == "F"
