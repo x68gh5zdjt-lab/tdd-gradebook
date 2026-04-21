964c88705b7ddfd0a466e3d5cb3d85c20070235b
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-04-21 08:39:32 -0400

    fixxed param

--- test_gradebook.py
+++ test_gradebook.py
@@ -7,7 +7,7 @@ def test_letter_grade_A():
 def test_letter_grade_F():
     assert letter_grade(42) == "F"
 
-@pytest.mark.paramitize("score, expected", [95, "A"], [85, "B"], [75, "C"], [65, "D"], [45, "F"])
-def test_letter_grade (score, expected):
+@pytest.mark.parametrize("score, expected", [(95, 'A'),(85, 'B'),(75, 'C'),(65, 'D'),(42, 'F')])
+def test_letter_grade(score, expected):
     assert letter_grade(score) == expected
 
