989a30285df6c991ea13653c53027431fa44811e
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-04-21 08:44:46 -0400

    funny number

--- test_gradebook.py
+++ test_gradebook.py
@@ -1,7 +1,7 @@
 from gradebook import letter_grade
 import pytest
 
-@pytest.mark.parametrize("score, expected", [(95, 'A'),(85, 'B'),(75, 'C'),(65, 'D'),(42, 'F')])
+@pytest.mark.parametrize("score, expected", [(95, 'A'),(85, 'B'),(75, 'C'),(67, 'D'),(42, 'F')])
 def test_letter_grade(score, expected):
     assert letter_grade(score) == expected
 
