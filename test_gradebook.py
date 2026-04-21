ea9e09466ce34e8ab00e38773f332022c29bc51b
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-04-21 08:42:18 -0400

    Removed redundent tests

--- test_gradebook.py
+++ test_gradebook.py
@@ -1,12 +1,6 @@
 from gradebook import letter_grade
 import pytest
 
-def test_letter_grade_A():
-    assert letter_grade(95) == "A"
-
-def test_letter_grade_F():
-    assert letter_grade(42) == "F"
-
 @pytest.mark.parametrize("score, expected", [(95, 'A'),(85, 'B'),(75, 'C'),(65, 'D'),(42, 'F')])
 def test_letter_grade(score, expected):
     assert letter_grade(score) == expected
