4060451ea1780d526a2b9bade8f49570e02d123f
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-04-21 08:36:45 -0400

    Added paramitize test

--- test_gradebook.py
+++ test_gradebook.py
@@ -1,8 +1,12 @@
 from gradebook import letter_grade
-from gradebook import is_passing
 
 def test_letter_grade_A():
     assert letter_grade(95) == "A"
 
 def test_letter_grade_F():
     assert letter_grade(42) == "F"
+
+@pytest.mark.paramitize("score, expected", [95, "A"], [85, "B"], [75, "C"], [65, "D"], [45, "F"])
+def test_letter_grade (score, expected):
+    assert letter_grade(score) == expected
+
